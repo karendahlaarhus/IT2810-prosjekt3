4ceea37adeac68ad6d2203ec021334a067ba4378
Johanna Sofie Slinde <JohannaSofie@dhcp-10-24-0-175.wlan.ntnu.no>
2020-10-13 09:57:45 +0200

    establish connection with db with auth #7

---
+++ prosjekt-3/src/backend/recipe.model.tsx
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let Recipe = new Schema({
+  id: {
+    type: Number,
+  },
+  title: {
+    type: String,
+  },
+  ingredients: [String],
+  servings: {
+    type: Number,
+  },
+  vegetarian: {
+    type: Boolean,
+  },
+  image: {
+    type: String,
+    format: URL,
+  },
+
+  //current average rating
+  rating: {
+    type: Number,
+  },
+
+  //number of ratings
+  userReviews: {
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model("Recipe", Recipe);
--- prosjekt-3/src/backend/server.js
+++ prosjekt-3/src/backend/server.js
@@ -14,9 +14,12 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://it2810-36.idi.ntnu.no:27017/recipedb", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  "mongodb://recipedb:recipedb@it2810-36.idi.ntnu.no:27017/?authSource=recipedb",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 const connection = mongoose.connection;
 
@@ -25,4 +28,40 @@ connection.once("open", function () {
 });
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
+});
+
+// SETUP ENDPOINTS WITH EXPRESS
+const recipeRoutes = express.Router();
+app.use("/recipe", recipeRoutes);
+
+// Delivering all available recipe items
+recipeRoutes.route("/").get(function (req, res) {
+  Recipe.find(function (err, recipe) {
+    if (err) {
+      console.log(err);
+    } else {
+      // all available added into a json format
+      res.json(recipe);
+    }
+  });
+});
+
+// This path extension is used to retrieve a todo item by providing an ID
+recipeRoutes.route("/:id").get(function (req, res) {
+  let id = req.params.id;
+  Recipe.findById(id, function (err, recipe) {
+    res.json(recipe);
+  });
+});
+
+// The route which is needed to be able to add new rating to a recipe item by sending a HTTP post request (/put)
+recipeRoutes.put("/:id/:rating").post(function (req, res) {
+  recipe
+    .save()
+    .then((recipe) => {
+      res.status(200).json({ _id: "new rating added" });
+    })
+    .catch((err) => {
+      res.status(400).send("adding new rating failed");
+    });
 });
