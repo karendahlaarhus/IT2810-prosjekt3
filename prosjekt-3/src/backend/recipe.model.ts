ca1a6373ff449a60cb1148a1d9065fee8f97fef2
Johanna Sofie Slinde <JohannaSofie@Johannas-MacBook-Air-2.local>
2020-10-16 13:58:01 +0200

    small fixes backend setup #7

---
+++ prosjekt-3/src/backend/recipe.model.ts
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+let Recipe = new Schema({
+  id: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  ingredients: [String],
+  preptime: {
+    type: Number
+  },
+  servings: {
+    type: Number,
+  },
+  instructions: {
+    type: String
+  },
+
+  tags: {
+    type: [String]
+  },
+
+  //current average rating
+  rating: {
+    type: Number,
+  },
+
+  //number of ratings
+  userReviews: {
+    type: Number,
+  },
+});
+
+export default mongoose.model("Recipe", Recipe, "Recipe");
---
+++ prosjekt-3/src/backend/server.ts
@@ -0,0 +1,52 @@
+/**
+ * With this code we’re creating an Express server, attaching the cors and body-parser middleware and making the server listening on port 4000
+ */
+
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import recipeRoutes from "./recipe"
+
+/**
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+ */
+
+const app = express();
+const PORT = 4000;
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(
+  "mongodb://recipedb:recipedb@it2810-36.idi.ntnu.no:27017/recipedb",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+
+const connection = mongoose.connection;
+connection.once("open", function () {
+  console.log("MongoDB database connection established successfully");
+});
+app.listen(PORT, function () {
+  console.log("Server is running on Port: " + PORT);
+});
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("We are on home");
+})
+
+
+// SETUP ENDPOINTS WITH EXPRESS
+app.use("/recipe", recipeRoutes);
+
+
+//Delivering all available items
+
+
