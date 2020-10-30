ea1a0337809718345176993b4a0c5e72fa54ed5f
karendaa <karendaa@stud.ntnu.no>
2020-10-30 09:42:32 +0100

    change from buttons to checkboxes for sorting

--- prosjekt-3/src/components/FilterBar.tsx
+++ prosjekt-3/src/components/FilterBar.tsx
@@ -1,8 +1,7 @@
 import { connect, useDispatch } from "react-redux";
 import React from "react";
 import { updateFilter } from "../store/actions/action";
-import Button from '@material-ui/core/Button';
-
+import { Checkbox, FormControlLabel } from "@material-ui/core";
 
 export const FilterBar = () => {
   const dispatch = useDispatch();
@@ -10,62 +9,35 @@ export const FilterBar = () => {
   return (
 
     <div className="filter-tags">
-        <Button
-            variant="contained"
-            color="default"
-            className='filterButton'
-            id='main'
-            value='main'
-            onClick={(e) => dispatch(updateFilter(e.currentTarget.value))}
-          > Main
-      </Button>
-      <Button
-            variant="contained"
-            color="default"
-            className='filterButton'
-            id='dessert'
-            value='dessert'
-            onClick={(e) => dispatch(updateFilter(e.currentTarget.value))}>
-        Dessert
-      </Button>
-      <Button
-            variant="contained"
-            color="default"
-            className='filterButton'
-            id='snack'
-            value='snack'
-            onClick={(e) => dispatch(updateFilter(e.currentTarget.value))}>  
-        Snack
-      </Button>
-      <Button
-            variant="contained"
-            color="default"
-            className='filterButton'
-            id='vegetarian'
-            value='vegetarian'
-            onClick={(e) => dispatch(updateFilter(e.currentTarget.value))}>
-        Vegetarian
-      </Button>
-      <Button
-            variant="contained"
-            color="default"
-            className='filterButton'
-            id='soup'
-            value='soup'
-            onClick={(e) => dispatch(updateFilter(e.currentTarget.value))}>
-        Soup
-      </Button>
-      <Button
-            variant="contained"
-            color="default"
-            className='filterButton'
-            id='soup'
-            value='soup'
-            onClick={(e) => dispatch(updateFilter(e.currentTarget.value))}>
-        Sides
-      </Button>
 
-
+      <FormControlLabel
+        id="main"
+        value="main"
+        control={<Checkbox color="primary" onChange={(e) => dispatch(updateFilter(e.currentTarget.value))}  />}
+        label="Main"
+        labelPlacement="end"
+      />
+      <FormControlLabel
+        id="vegetarian"
+        value="vegetarian"
+        control={<Checkbox color="primary" onClick={(e) => dispatch(updateFilter(e.currentTarget.value))}  />}
+        label="Vegetarian"
+        labelPlacement="end"
+      />
+      <FormControlLabel
+        id="dessert"
+        value="dessert"
+        control={<Checkbox color="primary"  onClick={(e) => dispatch(updateFilter(e.currentTarget.value))} />}
+        label="Dessert"
+        labelPlacement="end"
+      />
+      <FormControlLabel
+        id="soup"
+        value="soup"
+        control={<Checkbox color="primary" onClick={(e) => dispatch(updateFilter(e.currentTarget.value))} />}
+        label="Soup"
+        labelPlacement="end"
+      />
     </div>
     
   );
