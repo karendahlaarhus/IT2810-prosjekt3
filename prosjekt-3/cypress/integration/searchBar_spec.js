4446b9af50d308c750a20dbb5efe6a0c06416522
Johanna Sofie Slinde <JohannaSofie@Johannas-MacBook-Air-2.local>
2020-10-30 17:27:48 +0100

    add cypress test for searching #21

--- prosjekt-3/cypress/integration/searchBar_spec.js
+++ prosjekt-3/cypress/integration/searchBar_spec.js
@@ -1,26 +1,49 @@
 /*  Test for the SeachBar component */
 
+const { isContext } = require("vm");
+
 beforeEach(() => {
   cy.visit("/");
 });
 
-describe("Home Page", () => {
-  it("renders the frontpage", () => {
-    cy.visit("/");
-  });
+describe("Search in search field", () => {
+  context("User searching", () => {
+    it("renders the frontpage", () => {
+      cy.visit("/");
+    });
 
-  it("search field available", () => {
-    //cy.get('input[name=search for recipes]').type('pie')
-    cy.get("#searchBar");
-  });
+    it("search field available", () => {
+      //cy.get('input[name=search for recipes]').type('pie')
+      cy.get("#searchBar");
+    });
 
-  it("user can search in searchfield"),
-    () => {
-      cy.pause();
-      cy.get("#outlined-basic")
-        .type("pie")
-        .should(($TextField) => {
-          expect($TextField).to.have.length(3);
-        });
-    };
+    it("searches for a recipe", () => {
+      cy.get("#outlined-basic").type("pie").wait(1000);
+      cy.get(
+        ":nth-child(1) > :nth-child(1) > .MuiButtonBase-root > .MuiButton-label"
+      ).trigger("click");
+      cy.get('[style="display: flex;"] > .MuiTypography-root').should(
+        "contain",
+        "Pie"
+      );
+    });
+  });
+  context("User searching and filtering", () => {
+    it("searches and sets tags", () => {
+      cy.get("#outlined-basic").type("pie");
+      cy.get(
+        "#dessert > .MuiButtonBase-root > .MuiIconButton-label > .PrivateSwitchBase-input-8"
+      )
+        .trigger("click")
+        .wait(1500);
+      cy.get(
+        ":nth-child(2) > :nth-child(1) > .MuiButtonBase-root > .MuiButton-label"
+      ).trigger("click");
+      cy.get('[style="display: flex;"] > .MuiTypography-root').should(
+        "contain",
+        "Pie"
+      );
+      cy.get(".MuiDialogContent-root > :nth-child(4)").contains("dessert");
+    });
+  });
 });
